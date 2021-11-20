0b7577e285a1951ef9c4097dc593a0e816b0df56
murtaza199 <murtaza.sep@gmail.com>
2021-11-20 23:01:23 +0530

    time project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
-import Employee from "./Employee"
+import {useState} from "react"
 function App() {
+  let newTime= new Date().toLocaleTimeString()
+  const[cTime,setCtime]=useState(newTime)
+  function updateTime(){
+    newTime= new Date().toLocaleTimeString()
+    setCtime(newTime);
+  }
   return (
     <div className="App">
-      <Employee/>
+      <h1> {cTime} </h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
