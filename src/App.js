8e2cf762be98c2aafc0c0e250a77121f712301d1
murtaza199 <murtaza.sep@gmail.com>
2021-11-21 20:05:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 
 import './App.css';
-import {useState} from "react"
+import {Table} from "react-bootstrap"
 function App() {
-  let newTime= new Date().toLocaleTimeString()
-  const[cTime,setCtime]=useState(newTime)
-  function updateTime(){
-    newTime= new Date().toLocaleTimeString()
-    setCtime(newTime);
-  }
-  setInterval(updateTime,1000)
+  let arr =[
+    {name:"Murtaza" , email:"murtaza@test.com", addr:"Delhi", mobile:"111"},
+    {name:"Hassan" , email:"hassan@test.com", addr:"Noida", mobile:"222"},
+    {name:"Ansari" , email:"ansari@test.com", addr:"Godda", mobile:"333"},
+    {name:"Mohammad" , email:"mohammad@test.com", addr:"Nima", mobile:"444"},
+  ]
+  
+  
   return (
     <div className="App">
-      <h1> {cTime} </h1>
-      <button onClick={updateTime}>Get Time</button>
+      <h2>Employee Table </h2>
+      <Table striped bordered hover >
+        <tbody>
+          <tr>
+            <td>S.No</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Address</td>
+            <td>Mobile</td>
+            </tr>
+       {arr.map((data,i)=> <tr key={i}>
+         <td>{i+1}</td>
+        <td>{data.name} </td>
+        <td>{data.email} </td>
+        <td>{data.addr} </td>
+        <td>{data.mobile} </td>
+      </tr>)} 
+      </tbody>
+      </Table>
     </div>
   );
 }
